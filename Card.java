4e617543c739007f1c8cb1da66fb0b916f617530
lucht013 <lucht013>
2020-12-01 00:35:49 -0600

    Implement rank and redo value

--- Card.java
+++ Card.java
@@ -1,16 +1,22 @@
 public class Card {
     private String suit;
     private String color;
+    private int rank;
     private int value;
 
-    public Card(int value, String suit){
-        this.value = value;
+    public Card(int rank, String suit){
+        this.rank = rank;
         if(this.suit == "Club" || this.suit == "Spade"){
             color = "Black";
         }else if(this.suit == "Heart" || this.suit == "Diamond"){
             color = "Red";
         }else if(this.suit == "Joker"){
             color = "Joker";
+        }
+        if(rank > 10){
+            value = 10;
+        }else{
+            value = rank;
         }
     }
 
@@ -24,6 +30,10 @@ public class Card {
 
     public int getValue() {
         return value;
+    }
+
+    public int getRank() {
+        return rank;        
     }
 
 }
