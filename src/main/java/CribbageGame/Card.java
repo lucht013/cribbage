04aab7a463354bdf30efea4618cb813e6bf908e1
lucht013 <lucht013>
2020-12-26 21:45:35 -0600

    Implement Joker handler and card notes

--- src/main/java/CribbageGame/Card.java
+++ src/main/java/CribbageGame/Card.java
@@ -1,5 +1,15 @@
 package CribbageGame;
 public class Card {
+    /*
+     * Notes on Cards:
+     * Suits and colors are CAPITALIZED
+     * Suits are not pluralized
+     * cards are ranked 1 - 13
+     * Ranks above 10 are valued as 10, else rank == value
+     * Joker is colored 'Joker', suitted 'Joker', and rank/valued as -1
+     * if a card is malformed (ie. rank = 5, suit = joker) joker will override
+     */
+
     private String suit;
     private String color;
     private int rank;
@@ -8,7 +18,13 @@ public class Card {
     public Card(int rank, String suit){
         this.suit = suit;
         this.rank = rank;
+        //joker handler
+        if(rank == -1 || suit == "Joker"){
+            this.suit = "Joker";
+            this.rank = -1;
+        }
         
+        //Set color from suit
         if(this.suit == "Club" || this.suit == "Spade"){
             color = "Black";
         }else if(this.suit == "Heart" || this.suit == "Diamond"){
@@ -18,6 +34,8 @@ public class Card {
         }else{
             System.out.println("That's not a valid suit!");
         }
+
+        //set value from rank
         if(rank > 10){
             value = 10;
         }else{
